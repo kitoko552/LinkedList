c3893e9ad9827b6320119c9ef54b4a10d01482cf
Kosuke Kito <kito_kosuke@cyberagent.co.jp>
2015-04-17 13:33:51 +0900

    fix comment

--- LinkedList.h
+++ LinkedList.h
@@ -15,26 +15,26 @@ typedef struct node {
 
 /**
  * 新しいノードを作成する関数
- * @param data 最初に入れる値
- * @param *next 次のノードを指すポインタ
+ * @param int 最初に入れる値
+ * @param NODE* 次のノードを指すポインタ
  *
  * @return 新しく生成したノードのポインタ
  */
-NODE* newNode(int, NODE *);
+NODE* newNode(int, NODE*);
 
 /**
  * リストの先頭に要素を追加する関数
- * @param data 追加する値
- * @param **nodePointer リストの先頭をを指すポインタのポインタ
+ * @param int 追加する値
+ * @param NODE** リストの先頭をを指すポインタのポインタ
  *
  * @return 成功: true, 失敗: false
  */
-bool prependNode(int, NODE **);
+bool prependNode(int, NODE**);
 
 /**
  * リストの末尾に要素を追加する関数
- * @param data 追加する値
- * @param **nodePointer リストの先頭をを指すポインタのポインタ
+ * @param int 追加する値
+ * @param NODE** リストの先頭をを指すポインタのポインタ
  *
  * @return 成功: true, 失敗: false
  */
@@ -42,9 +42,9 @@ bool appendNode(int, NODE**);
 
 /**
  * リストの指定した位置に要素を追加する関数
- * @param index 追加する位置
- * @param data 追加する値
- * @param **nodePointer リストの先頭をを指すポインタのポインタ
+ * @param int 追加する位置
+ * @param int 追加する値
+ * @param NODE** リストの先頭をを指すポインタのポインタ
  *
  * @return 成功: true, 失敗: false
  */
@@ -52,8 +52,8 @@ bool insertNode(int, int, NODE**);
 
 /**
  * リストの指定した位置の要素を削除する関数
- * @param index 削除する位置
- * @param **nodePointer リストの先頭をを指すポインタのポインタ
+ * @param int 削除する位置
+ * @param NODE** リストの先頭をを指すポインタのポインタ
  *
  * @return 成功: true, 失敗: false
  */
@@ -61,9 +61,9 @@ bool deleteNode(int, NODE**);
 
 /**
  * リストの指定した位置の要素を変更する関数
- * @param index 変更する位置
- * @param data 変更後の値
- * @param **nodePointer リストの先頭をを指すポインタのポインタ
+ * @param int 変更する位置
+ * @param int 変更後の値
+ * @param NODE** リストの先頭をを指すポインタのポインタ
  *
  * @return 成功: true, 失敗: false
  */
@@ -71,7 +71,7 @@ bool replaceNode(int, int, NODE**);
 
 /**
  * リストの要素数を取得する関数
- * @param *nodePointer リストの先頭をを指すポインタ
+ * @param NODE* リストの先頭をを指すポインタ
  *
  * @return 要素数
  */
@@ -79,8 +79,8 @@ int getNumNodes(NODE*);
 
 /**
  * 指定した値を検索する関数
- * @param data 検索する値
- * @param *nodePointer 検索対象のリストを指すポインタ
+ * @param int 検索する値
+ * @param NODE* 検索対象のリストを指すポインタ
  *
  * @return 指定した値が存在すればindex、存在しなければ-1を返す。
  */
@@ -88,13 +88,13 @@ int searchData(int, NODE*);
 
 /**
  * リストの全要素を削除する関数
- * @param **nodePointer リストの先頭をを指すポインタのポインタ
+ * @param NODE** リストの先頭をを指すポインタのポインタ
  */
 void deleteAllNodes(NODE**);
 
 /**
  * リストの全要素を標準出力に出力する関数
- * @param *nodePointer リストの先頭をを指すポインタ
+ * @param NODE* リストの先頭をを指すポインタ
  */
 void printList(NODE*);
 
